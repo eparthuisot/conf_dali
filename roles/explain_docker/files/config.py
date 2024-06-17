9c625f5bda68d587cc70594ae3c52df36d167ccd
Eric PARTHUISOT <eric@ma-luna.net>
2024-06-17 15:13:47 +0200

    Force ssl connect from explain to db postgresql

--- roles/explain_docker/files/config.py
+++ roles/explain_docker/files/config.py
@@ -6,7 +6,7 @@ DB_USER = os.environ.get('DB_USER', 'postgres')
 DB_PASS = os.environ.get('DB_PASS', 'postgres')
 DB_SERVICE = os.environ.get('DB_SERVICE', '0.0.0.0')
 DB_PORT = os.environ.get('DB_PORT', 5432)
-SQLALCHEMY_DATABASE_URI = 'postgresql://{0}:{1}@{2}:{3}/{4}'.format(
+SQLALCHEMY_DATABASE_URI = 'postgresql://{0}:{1}@{2}:{3}/{4}?sslmode=require'.format(
     DB_USER, DB_PASS, DB_SERVICE, DB_PORT, DB_NAME
 )
 DEBUG_TB_INTERCEPT_REDIRECTS = False
